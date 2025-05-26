1d56177e36914ea18acea274cbc7c71e27276c8a
Bill Guo <youxguo@gmail.com>
2025-05-26 12:38:52 -0400

    Added check function for goal state Changed heuristic functions to accept arrays

--- Eight_square_puzzle.py
+++ Eight_square_puzzle.py
@@ -20,38 +20,48 @@ class EightSquare:
         self.puzzle[2][1] = num7
         self.puzzle[2][2] = num8
 
-    def calculate_heuristic_one(self) -> int:
+    def calculate_heuristic_one(self, array) -> int:
         """Heuristic 1 gets the number of misplaced tiles in the current state"""
         incorrect_tiles: int = 0
         for rows in range(3):
             for columns in range(3):
-                if self.puzzle[rows][columns] != rows+columns:
+                if array[rows][columns] != rows+columns:
                     incorrect_tiles += 1
         return incorrect_tiles
 
-    def calculate_heuristic_two(self) -> int:
+    def calculate_heuristic_two(self, array) -> int:
         """Heruistic 2 gets the Manhattan distance for all times from its correct location"""
         sum_of_distance: int = 0
         for rows in range(3):
             for columns in range(3):
-                if self.puzzle[rows][columns] == 0:
+                if array[rows][columns] == 0:
                     sum_of_distance = sum_of_distance + abs(rows - 0) + abs(columns - 0)
-                elif self.puzzle[rows][columns] == 1:
+                elif array[rows][columns] == 1:
                     sum_of_distance = sum_of_distance + abs(rows - 0) + abs(columns - 1)
-                elif self.puzzle[rows][columns] == 2:
+                elif array[rows][columns] == 2:
                     sum_of_distance = sum_of_distance + abs(rows - 0) + abs(columns - 2)
-                elif self.puzzle[rows][columns] == 3:
+                elif array[rows][columns] == 3:
                     sum_of_distance = sum_of_distance + abs(rows - 1) + abs(columns - 0)
-                elif self.puzzle[rows][columns] == 4:
+                elif array[rows][columns] == 4:
                     sum_of_distance = sum_of_distance + abs(rows - 1) + abs(columns - 1)
-                elif self.puzzle[rows][columns] == 5:
+                elif array[rows][columns] == 5:
                     sum_of_distance = sum_of_distance + abs(rows - 1) + abs(columns - 2)
-                elif self.puzzle[rows][columns] == 6:
+                elif array[rows][columns] == 6:
                     sum_of_distance = sum_of_distance + abs(rows - 2) + abs(columns - 0)
-                elif self.puzzle[rows][columns] == 7:
+                elif array[rows][columns] == 7:
                     sum_of_distance = sum_of_distance + abs(rows - 2) + abs(columns - 1)
-                elif self.puzzle[rows][columns] == 8:
+                elif array[rows][columns] == 8:
                     sum_of_distance = sum_of_distance + abs(rows - 2) + abs(columns - 2)
 
         return sum_of_distance
 
+    def check_if_goal(self) -> bool:
+        """Checks the puzzle to see if we have completed it"""
+        reached_goal: bool = True
+        while reached_goal:
+            for rows in range(3):
+                for columns in range(3):
+                    reached_goal = self.puzzle[rows][columns] == (rows+columns)
+
+        return reached_goal
+                
